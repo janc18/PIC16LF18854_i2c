cfa430eb8886a7ae1001ae6685cd676caf26f4c7
jose <josecla011@gmail.com>
2022-01-25 23:09:24 -0600

    First commit

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * File:   main.c
+ * Author: jose
+ *
+ * Created on 23 de enero de 2022, 08:51 PM
+ */
+
+
+#include <xc.h>
+#include  "config.h"
+#define _XTAL_FREQ 32000000
+void main(void) {
+    return; 
+}
